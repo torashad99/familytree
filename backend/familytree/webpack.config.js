302b9aa3b8ecf4d2f696664d8caf00dcddabc1cb
Catherine Chen <bluebirid228@gmail.com>
2021-08-05 11:00:38 -0700

    update configuration

--- backend/familytree/webpack.config.js
+++ backend/familytree/webpack.config.js
@@ -4,14 +4,20 @@ module.exports = {
         filename: 'react-app.js'
     },
     module: {
-        rules: [{
-            test: /\.(js|jsx)$/,
-            exclude: /node_modules/,
-            loader: "babel-loader",
-            options: {
-                presets: ['@babel/preset-env', '@babel/preset-react']
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                loader: "babel-loader",
+                options: {
+                    presets: ['@babel/preset-env', '@babel/preset-react']
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
             }
-        }]
+        ]
     },
     resolve: {
         extensions: ['.js', '.jsx']
